eb4c5d0b07ff9b6420d5c35eb1fb97722bf1a860
slimesown <plee50@binghamton.edu>
2023-04-20 13:49:31 -0400

    dog starts and ends as standing. audio changes
    Co-authored-by: mikebednarz <mikebednarz@users.noreply.github.com>

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const runScript = function () {
   dogCage.style.position = "fixed";
   dogCage.className = "dog-cage-item";
   dogCage.style.bottom = "0";
-  dogCage.style.width = "1600px";
+  dogCage.style.width = "2000px";
   dogCage.style.height = "80px";
 
   dogInject.src = chrome.runtime.getURL("pictures/dog-midwalk.png");
@@ -26,7 +26,14 @@ const runScript = function () {
 runScript();
 
 function bark() {
+  const barkPicker = Math.floor(Math.random() * 3);
+  const barks = [
+    "pictures/bark.mp3",
+    "pictures/bark1.mp3",
+    "pictures/bark2.mp3",
+  ];
   const barked = document.querySelector(".bark-audio");
+  barked.src = chrome.runtime.getURL(barks[barkPicker]);
   barked.play();
 }
 
@@ -35,12 +42,13 @@ function move() {
   const dog = document.querySelector(".dog-pic-item");
   const cage = document.querySelector(".dog-cage-item");
   const cageWidth = parseInt(cage.style.width);
-  const dogWidth = parseInt(dog.style.width);
   let pos = 0;
   clearInterval(id);
-  id = setInterval(frame, 10);
+  id = setInterval(frame, 7);
   function frame() {
-    if (pos == cageWidth - dogWidth) {
+    if (pos == cageWidth) {
+      dog.style.left = "0px";
+      dog.src = chrome.runtime.getURL("pictures/dog-midwalk.png");
       i = 0;
       clearInterval(id);
       clearInterval(id2);
@@ -50,13 +58,13 @@ function move() {
     }
   }
 
-  let i = 0;
+  let i = 1;
   const pics = ["pictures/dog-midwalk.png", "pictures/dog-hop2.png"];
   function toggle() {
     dog.src = chrome.runtime.getURL(pics[i]);
     i = (i + 1) % pics.length;
   }
-  let id2 = setInterval(toggle, 700);
+  let id2 = setInterval(toggle, 400);
 }
 
 const dog = document.querySelector(".dog-pic-item");
